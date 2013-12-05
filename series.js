342eaf58829d2f378c47f349edfd4aa3d901a137
Yoann Ramos <yramos@epsi.fr>
2013-12-05 18:57:53 +0100

    Ajout jquery en local

---
+++ series.js
@@ -0,0 +1,10 @@
+
+/**
+ * Created by Yoann on 05/12/13.
+ */
+function openMoreFilter() {
+    if($('#filtres-additionnels').attr('display','none'))
+        $('#filtres-additionnels').fadeIn();
+    else
+        $('#filtres-additionnels').fadeOut();
+}
